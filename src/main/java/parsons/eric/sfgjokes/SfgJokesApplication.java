6f97f16965138fe53d38f6263c63dbc4b198a4c4
Eric Parsons <eparsons@updox.com>
2022-11-29 14:47:33 -0500

    feat: init project from Spring Initializr

---
+++ src/main/java/parsons/eric/sfgjokes/SfgJokesApplication.java
@@ -0,0 +1,13 @@
+package parsons.eric.sfgjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/parsons/eric/sfgjokes/SfgJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package parsons.eric.sfgjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
